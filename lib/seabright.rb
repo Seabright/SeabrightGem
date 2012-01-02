b5bebf63380182fd9d9c8180ef96a57854eca907
John Bragg <remotezygote@gmail.com>
2012-01-02 13:55:10 -0800

    proper autoload syntax

--- lib/seabright.rb
+++ lib/seabright.rb
@@ -1,7 +1,7 @@
 module Seabright
-  autoload :Base "seabright/base"
-  autoload :ClassFactory "seabright/class_factory"
-  autoload :Stylesheet "seabright/stylesheet"
-  autoload :Javascript "seabright/javascript"
-  autoload :Image "seabright/image"  
+  autoload :Base, "seabright/base"
+  autoload :ClassFactory, "seabright/class_factory"
+  autoload :Stylesheet, "seabright/stylesheet"
+  autoload :Javascript, "seabright/javascript"
+  autoload :Image, "seabright/image"  
 end
